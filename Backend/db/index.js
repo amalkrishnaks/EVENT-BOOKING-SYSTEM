5d7a5803b1b692c034ca0e4bb62289b8c9879ccf
Amal <amal@gmail.com>
2025-06-12 23:27:09 +0530

    server is  commit

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,13 +1,24 @@
 const mongoose=require('mongoose');
 require('dotenv').config();
 
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log('Connected to EventBookingDB');
-  })
-  .catch(e => {
-    console.log(e);
-  });
+
+// mongoose
+//   .connect(process.env.MONGODB_URL)
+//   .then(() => {
+//     console.log('Connected to EventBookingDB');
+//   })
+//   .catch(e => {
+//     console.log(e);
+//   });
+    try{
+    mongoose
+    .connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB database is connected');
+    } catch (err) {
+        console.error('MongoDB database connection failed', err);
+    }
 
 module.exports=mongoose;
--- Backend/server.js
+++ Backend/server.js
@@ -1,20 +1,20 @@
 const express=require('express');
 const cors=require('cors');
 require('dotenv').config();
-
 const app=express();
 const port=process.env.PORT || 4000 ;
 
+const corsOptions={
+    origin: "https://event-booking-system-tau.vercel.app",
+    methods:'GET,POST,PUT,DELETE,PATCH'
+};
 
-//middlewares
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static('public'));
 
-//db
 require('./db');
 
-//routes
 const routes=require('./routes');
 app.use('/api',routes);
 
@@ -26,5 +26,5 @@ app.use('*',(req,res)=>{
 
 
 app.listen(port,()=>{
-    console.log(`Backend App Is Running`);
+    console.log(`app is running `);
 })
