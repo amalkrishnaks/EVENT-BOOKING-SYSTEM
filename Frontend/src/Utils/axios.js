67e5813b7f6bf76272d980e5c395d10c86dd5925
Amal <amal@gmail.com>
2025-06-12 22:42:32 +0530

    four push

--- Frontend/src/Utils/axios.js
+++ Frontend/src/Utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance=axios.create({
-      baseURL:'https://event-booking-system-rfca.onrender.com/api',
+      baseURL:'https://event-booking-system-rfca.onrender.com',
       timeout:150000,
 });
 
