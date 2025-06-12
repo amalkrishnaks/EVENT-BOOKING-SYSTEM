59a2472f3e68d4b0c02b8ecd3737131b555fb634
Amal <amal@gmail.com>
2025-06-12 22:29:09 +0530

    server commit

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 require('dotenv').config();
 
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGODB_URL)
   .then(() => {
     console.log('Connected to EventBookingDB');
   })
