7a1f354a1b91e0dcdc4bc08e25f2fd307eac67df
Amal <amal@gmail.com>
2025-06-12 13:47:19 +0530

    server commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,20 @@
 const express=require('express');
 const cors=require('cors');
 require('dotenv').config();
+
 const app=express();
 const port=process.env.PORT || 4000 ;
 
 
-
+//middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+//db
 require('./db');
 
+//routes
 const routes=require('./routes');
 app.use('/api',routes);
 
@@ -23,5 +26,5 @@ app.use('*',(req,res)=>{
 
 
 app.listen(port,()=>{
-    console.log(`app is running @ http://localhost:${port}/`);
+    console.log(`Backend App Is Running`);
 })
