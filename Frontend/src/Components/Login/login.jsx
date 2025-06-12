b5bdce7966696b684b1569fd99033c0de48e1ec0
Amal <amal@gmail.com>
2025-06-12 23:37:43 +0530

    six  push again again

--- Frontend/src/Components/Login/login.jsx
+++ Frontend/src/Components/Login/login.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from '../../Utils/axios';
 import { ToastContainer,toast } from 'react-toastify';
+import BASE_URL from '../../Utils/axios';
 import './login.css';
 import Events from '../../pages/Events/events';
 
@@ -22,13 +23,13 @@ const Login=({setShowLogin})=>{
     const onClick=async()=>{
         try {
             if(currState==='Login'){
-                const response=await axios.post('/user/login',login)
+                const response=await axios.post(`${BASE_URL}/user/login`,login)
                 localStorage.setItem('token',response.data.token);
                 setShowLogin(false)
                 navigate('/');
                 console.log(response);
             }else{
-                const response=await axios.post('/user/signup',login)
+                const response=await axios.post(`${BASE_URL}/user/signup`,login)
                 console.log(response.data);
                 toast.success("Registeration successful!");
             }
