76187b25bac0e9e690c411c43c620ab4f11b414b
Amal <amal@gmail.com>
2025-06-13 00:34:53 +0530

    backend is  commited

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -2,23 +2,23 @@ const mongoose=require('mongoose');
 require('dotenv').config();
 
 
-// mongoose
-//   .connect(process.env.MONGODB_URL)
-//   .then(() => {
-//     console.log('Connected to EventBookingDB');
-//   })
-//   .catch(e => {
-//     console.log(e);
-//   });
-    try{
-    mongoose
-    .connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB database is connected');
-    } catch (err) {
-        console.error('MongoDB database connection failed', err);
-    }
+mongoose
+  .connect("mongodb://localhost:27017/Event-Booking-DB-3")
+  .then(() => {
+    console.log('Connected to EventBookingDB');
+  })
+  .catch(e => {
+    console.log(e);
+  });
+    // try{
+    // mongoose
+    // .connect(process.env.MONGO_URL, {
+    //         useNewUrlParser: true,
+    //         useUnifiedTopology: true,
+    //     });
+    //     console.log('MongoDB database is connected');
+    // } catch (err) {
+    //     console.error('MongoDB database connection failed', err);
+    // }
 
 module.exports=mongoose;
